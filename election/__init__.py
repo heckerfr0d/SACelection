34c5dd5ae81ab52c8f86d86a5b523b59a8a4b8f8
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2021-11-30 16:43:12 +0530

    cors stuf

--- election/__init__.py
+++ election/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_login import LoginManager
 from . import db
 import os
 import datetime
 
 loginmanager = LoginManager()
+cors = CORS()
 
 def init_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default')
     app.config['DATABASE_URL'] = os.getenv('DATABASE_URL', 'dbname=test')
+    app.config['CORS_HEADERS'] = 'Content-Type'
     loginmanager.init_app(app)
+    cors.init_app(app)
     db.init_app(app)
 
 
