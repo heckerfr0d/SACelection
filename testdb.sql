8543d2a3559bcd387c49e8f65e5e2b5e6ff398dc
randomdeveloper7 <harimurali_b190547cs@nitc.ac.in>
2021-11-30 13:07:47 +0530

    exclusive admin

--- testdb.sql
+++ testdb.sql
@@ -36,11 +36,12 @@
   );
 
   -- insert dummy data into tables
-  INSERT INTO users (email_id, password, status) VALUES ('hadif_b190513cs@nitc.ac.in', 'd30e375bd0da079acfecfe8206e21c62fc3136a35def934dce52150db4f3c135', -1);
-  INSERT INTO users (email_id, password, status) VALUES ('hanna_b190420cs@nitc.ac.in', '086b42dd0491ea53914359b962dfe56dd17a1bcaaf9858ced0f9bccaef065d50', -1);
-  INSERT INTO users (email_id, password, status) VALUES ('harimurali_b190547cs@nitc.ac.in', '8b0b28c15c2da3a92a41019d86aaa26328a76508dd80dfb87230fa878f3254c8', -1);
-  INSERT INTO users (email_id, password, status) VALUES ('faseem_b190515cs@nitc.ac.in', 'd206c5d8b17a4f0d61b26641cc9bc2d4e3ccda4a8877d92158a1b43395b7471c', -1);
-  INSERT INTO users (email_id, password, status) VALUES ('abhinav_b190461cs@nitc.ac.in', '86a8157869152c00b3a336aa148c1d8dd8e7c2c0a2fa2b6177ccfcaf83f503cf', -1);
+  INSERT INTO users (email_id, password, status) VALUES ('admin@nitc.ac.in', 'fb001dfcffd1c899f3297871406242f097aecf1a5342ccf3ebcd116146188e4b', -1);
+  INSERT INTO users (email_id, password, status) VALUES ('hadif_b190513cs@nitc.ac.in', 'd30e375bd0da079acfecfe8206e21c62fc3136a35def934dce52150db4f3c135', 0);
+  INSERT INTO users (email_id, password, status) VALUES ('hanna_b190420cs@nitc.ac.in', '086b42dd0491ea53914359b962dfe56dd17a1bcaaf9858ced0f9bccaef065d50', 0);
+  INSERT INTO users (email_id, password, status) VALUES ('harimurali_b190547cs@nitc.ac.in', '8b0b28c15c2da3a92a41019d86aaa26328a76508dd80dfb87230fa878f3254c8', 0);
+  INSERT INTO users (email_id, password, status) VALUES ('faseem_b190515cs@nitc.ac.in', 'd206c5d8b17a4f0d61b26641cc9bc2d4e3ccda4a8877d92158a1b43395b7471c', 0);
+  INSERT INTO users (email_id, password, status) VALUES ('abhinav_b190461cs@nitc.ac.in', '86a8157869152c00b3a336aa148c1d8dd8e7c2c0a2fa2b6177ccfcaf83f503cf', 0);
   INSERT INTO users (email_id, password, status) VALUES ('sinadin_b190534cs@nitc.ac.in', '75eb603c432f79e9d878ce455495f8d240975b2fcf8b258b4b9725dbe18abf3e', 0);
   INSERT INTO users (email_id, password, status) VALUES ('midhunkumar_b190439cs@nitc.ac.in', '316662b5abd17ba00c0710021ceb6a108b288fa1784eee76377265518c8234d9', 0);
   INSERT INTO users (email_id, password, status) VALUES ('jefin_b190138ce@nitc.ac.in', 'cd682626b57a7b03d5099392b33b529833f5978f18f2b763740878cb300ed7fd', 0);
