ef60a730bfe2ffa3cdaa3ee6a928c22f5b0c83a4
heckerfr0d <hadif_b190513cs@nitc.ac.in>
2021-12-03 15:58:06 +0530

    merge conflicts resolved (hopefully without breaking anything)

--- election/db.py
+++ election/db.py
@@ -5,7 +5,7 @@ import hashlib
 
 
 def get_db():
-    #function to connect to the database
+    # function to connect to the database
     if 'db' not in g:
         DATABASE_URL = current_app.config['DATABASE_URL']
         # g.db = connect(DATABASE_URL, sslmode='require')
@@ -14,7 +14,7 @@ def get_db():
 
 
 def close_db(e=None):
-    #function to close connection to the database
+    # function to close connection to the database
     db = g.pop('db', None)
     if db is not None:
         db.close()
@@ -25,7 +25,7 @@ def init_app(app):
 
 
 class User:
-    #class represeting the user, containg all their data and functions to retrieve it
+    # class represeting the user, containg all their data and functions to retrieve it
     def __init__(self, email_id, password, status):
         self.email_id = email_id
         self.password = password
@@ -49,25 +49,25 @@ class User:
 
 
 def hash_password(password):
-    #hashing the password to store it securely
+    # hashing the password to store it securely
     return hashlib.sha3_256(password.encode()).hexdigest()
 
 
 def get_user(email):
-    #getting user details
-    db = get_db() # connecting to  the database
-    cur = db.cursor() #initialising a cursor
+    # getting user details
+    db = get_db()  # connecting to  the database
+    cur = db.cursor()  # initialising a cursor
     cur.execute(
-        "SELECT email_id, password, status FROM users WHERE email_id=%s", (email,)) #executing the sql query
-    user = cur.fetchone() # fetching the first result
+        "SELECT email_id, password, status FROM users WHERE email_id=%s", (email,))  # executing the sql query
+    user = cur.fetchone()  # fetching the first result
     cur.close()
-    if user: #checking if an user has been intialised
-        return User(user[0], user[1], int(user[2])) #returning user data
+    if user:  # checking if an user has been intialised
+        return User(user[0], user[1], int(user[2]))  # returning user data
     return None
 
 
 def auth(email_id, password):
-    #function to authenticate user credentials
+    # function to authenticate user credentials
     user = get_user(email_id)
     if user:
         if user.password == hash_password(password):
@@ -76,7 +76,7 @@ def auth(email_id, password):
 
 
 def add_user(email_id, password, status):
-    #function to add a new user to the db
+    # function to add a new user to the db
     user = get_user(email_id)
     if user is None:
         db = get_db()
@@ -84,14 +84,14 @@ def add_user(email_id, password, status):
         password = hash_password(password)
         cur.execute("INSERT INTO users (email_id, password, status) VALUES (%s, %s, %s)",
                     (email_id, password, status))
-        db.commit() # commiting the change to the db 
+        db.commit()  # commiting the change to the db
         cur.close()
         return User(email_id, password, status)
     return None
 
 
 def set_election(start_datetime, end_datetime, admin):
-    #function to create a new election instance
+    # function to create a new election instance
     db = get_db()
     cur = db.cursor()
     cur.execute("INSERT INTO election (start_datetime, end_datetime, admin_email) VALUES (%s, %s, %s)",
@@ -101,7 +101,7 @@ def set_election(start_datetime, end_datetime, admin):
 
 
 def modify_election(start_datetime, end_datetime, eid):
-    #function to modify the election start and end time
+    # function to modify the election start and end time
     db = get_db()
     cur = db.cursor()
     cur.execute("UPDATE election SET start_datetime=%s, end_datetime=%s WHERE election_id=%s",
@@ -111,19 +111,19 @@ def modify_election(start_datetime, end_datetime, eid):
 
 
 def get_running_election():
-    #function to get the active election
+    # function to get the active election
     db = get_db()
     cur = db.cursor()
     cur.execute("SELECT election_id FROM election WHERE start_datetime<=%s AND end_datetime>=%s",
                 (datetime.now(), datetime.now()))
-    #datetime.now returns the current local time 
+    # datetime.now returns the current local time
     election = cur.fetchone()
     cur.close()
     return election
 
 
 def get_upcoming_election():
-    #function to get the details of the upcoming election
+    # function to get the details of the upcoming election
     db = get_db()
     cur = db.cursor()
     cur.execute(
@@ -134,7 +134,7 @@ def get_upcoming_election():
 
 
 def get_upcoming_election_details():
-    #function to get the details of the upcoming election
+    # function to get the details of the upcoming election
     db = get_db()
     cur = db.cursor()
     cur.execute("SELECT * FROM election WHERE start_datetime>%s",
@@ -145,7 +145,7 @@ def get_upcoming_election_details():
 
 
 def get_positions():
-    #function to get the positon details from the database
+    # function to get the positon details from the database
     db = get_db()
     cur = db.cursor()
     cur.execute("SELECT position,position_id FROM position ORDER BY position_id")
@@ -153,89 +153,90 @@ def get_positions():
     cur.close()
     return positions
 
-def cur_candidates(id,elec_id):
-    #function to get the candidatees of  a certain positon and a certain election
+
+def cur_candidates(id, elec_id):
+    # function to get the candidatees of  a certain positon and a certain election
     db = get_db()
     cur = db.cursor()
-    cur.execute("SELECT name,email_id FROM candidate where position_id=%s AND election_id=%s ORDER BY position_id, name",(id,elec_id))
+    cur.execute("SELECT name,email_id FROM candidate where position_id=%s AND election_id=%s ORDER BY position_id, name", (id, elec_id))
     candidates = cur.fetchall()
     cur.close()
     return candidates
 
-def cur_candidate_votes(id,elec_id):
-    #function to get the number of votes a candidate has recieved
+
+def cur_candidate_votes(id, elec_id):
+    # function to get the number of votes a candidate has recieved
     db = get_db()
     cur = db.cursor()
-    cur.execute("SELECT name,votes FROM candidate where position_id=%s AND election_id=%s",(id,elec_id))
+    cur.execute(
+        "SELECT name,votes FROM candidate where position_id=%s AND election_id=%s", (id, elec_id))
     candidates = cur.fetchall()
     cur.close()
     return candidates
 
-def add_candidate(name,email,position):
-    #function to add a candidate to the db
+
+def add_candidate(name, email, position):
+    # function to add a candidate to the db
     db = get_db()
     cur = db.cursor()
-    election_id=get_upcoming_election()
-    cur.execute("INSERT INTO candidate(email_id,name,votes,position_id,election_id) VALUES (%s,%s,0,%s,%s)",(email,name,position,election_id[0]))
+    election_id = get_upcoming_election()
+    cur.execute("INSERT INTO candidate(email_id,name,votes,position_id,election_id) VALUES (%s,%s,0,%s,%s)",
+                (email, name, position, election_id[0]))
     db.commit()
     cur.close()
+
 
 def delete_candidate(email):
-    #function to delete a candidate from the db
+    # function to delete a candidate from the db
     db = get_db()
     cur = db.cursor()
-    cur.execute("DELETE FROM candidate WHERE email_id=%s",(email,))
+    cur.execute("DELETE FROM candidate WHERE email_id=%s", (email,))
     db.commit()
     cur.close()
+
 
 def delete_all_candidates(id):
-    #function to delete all the candidate  competing for a specific positoin
+    # function to delete all the candidate  competing for a specific positoin
     db = get_db()
     cur = db.cursor()
-    cur.execute("DELETE FROM candidate WHERE position_id=%s",(id,))
+    cur.execute("DELETE FROM candidate WHERE position_id=%s", (id,))
     db.commit()
     cur.close()
 
-def modify_votes(can_email,user_id,elec_id):
-    #function to update the vote count and the update the vote for table 
+
+def modify_votes(can_email, user_id, elec_id):
+    # function to update the vote count and the update the vote for table
     db = get_db()
     cur = db.cursor()
-<<<<<<< HEAD
-    cur.execute("UPDATE candidate SET votes=votes+1 where email_id=%s AND election_id=%s",(can_email,elec_id)) #updating vote count
-    cur.execute("INSERT INTO votes_for (voter_email,position_id) VALUES (%s,(SELECT position_id FROM candidate where email_id=%s AND election_id=%s));",(user_id,can_email,elec_id)) #updating votes_for table
-=======
     try:
-        cur.execute("INSERT INTO votes_for (voter_email,position_id, election_id) VALUES (%s,(SELECT position_id FROM candidate where name=%s AND election_id=%s), %s);",(user_id,can_name,elec_id, elec_id))
-        cur.execute("UPDATE candidate SET votes=votes+1 where name=%s AND election_id=%s",(can_name,elec_id))
+        cur.execute("INSERT INTO votes_for (voter_email,position_id, election_id) VALUES (%s,(SELECT position_id FROM candidate where email_id=%s AND election_id=%s), %s);",
+                    (user_id, can_email, elec_id, elec_id))
+        cur.execute(
+            "UPDATE candidate SET votes=votes+1 where email_id=%s AND election_id=%s", (can_email, elec_id))
     except:
         return "error: kallavote"
->>>>>>> 94c3b58 (conflicting commit lol)
     db.commit()
     cur.close()
 
     return "done"
 
-<<<<<<< HEAD
-def check_if_voted(user_id,position_id):
-    #functin to check if a user has voted for a certain position
-    db = get_db()
-    cur = db.cursor()
-    cur.execute("SELECT * FROM votes_for WHERE voter_email=%s AND position_id=%s",(user_id,position_id))
-=======
-def check_if_voted(user_id,position_id, election_id):
+
+def check_if_voted(user_id, position_id, election_id):
+    # function to check if a user has voted for a position
     db = get_db()
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM votes_for WHERE voter_email=%s AND position_id=%s AND election_id=%s",(user_id,position_id, election_id))
+    cur.execute("SELECT * FROM votes_for WHERE voter_email=%s AND position_id=%s AND election_id=%s",
+                (user_id, position_id, election_id))
 
->>>>>>> 94c3b58 (conflicting commit lol)
     voted = cur.fetchall()
     cur.close()
 
     return voted
 
+
 def get_elections():
-    #function to get all elections
+    # function to get all elections
     db = get_db()
     cur = db.cursor()
     cur.execute("SELECT * FROM election")
@@ -244,19 +245,21 @@ def get_elections():
     return elections
 
 
-def hide_results(): # function to check if we should display the results button
-    if get_running_election() or not get_elections(): # checking if there is an active election or the absence of a completed one
+def hide_results():  # function to check if we should display the results button
+    # checking if there is an active election or the absence of a completed one
+    if get_running_election() or not get_elections():
         return True
     return False
 
+
 def get_results():
-    #function to get the results of the election
+    # function to get the results of the election
     db = get_db()
     cur = db.cursor()
-    cur.execute("SELECT c.name, c.votes, p.position  FROM candidate c, position p WHERE c.election_id=(SELECT election_id FROM election WHERE end_datetime=(SELECT MAX(end_datetime) FROM election WHERE end_datetime<%s)) AND c.position_id=p.position_id AND c.votes=(SELECT MAX(votes) FROM candidate WHERE position_id=c.position_id) ORDER BY p.position_id, c.name",(datetime.now(),))
+    cur.execute("SELECT c.name, c.votes, p.position  FROM candidate c, position p WHERE c.election_id=(SELECT election_id FROM election WHERE end_datetime=(SELECT MAX(end_datetime) FROM election WHERE end_datetime<%s)) AND c.position_id=p.position_id AND c.votes=(SELECT MAX(votes) FROM candidate WHERE position_id=c.position_id) ORDER BY p.position_id, c.name", (datetime.now(),))
     results = cur.fetchall()
     cur.close()
     packed = {}
-    for name,votes,position in results: #adding the results to a data structure
-        packed[position] = packed.get(position,[]) + [(name,votes)]
+    for name, votes, position in results:  # adding the results to a data structure
+        packed[position] = packed.get(position, []) + [(name, votes)]
     return packed
